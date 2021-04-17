3b0f0b1714b3be501f4b04329a871040129f8836
MINIMgun <56129455+MINIMgun@users.noreply.github.com>
2021-04-17 16:54:21 +0200

    Created project using spring initializr

---
+++ src/main/java/net/minimgun/quizframework/QuizframeworkApplication.java
@@ -0,0 +1,13 @@
+package net.minimgun.quizframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizframeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/minimgun/quizframework/QuizframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package net.minimgun.quizframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
